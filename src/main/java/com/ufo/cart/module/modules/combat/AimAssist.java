0c167a5d2958aca1b8055fff6396bb091c032b32
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-12-13 05:16:37 -0800

    Added FOV and Range to aimassists
    Sponsored by chatgpt :money_mouth:

--- src/main/java/com/ufo/cart/module/modules/combat/AimAssist.java
+++ src/main/java/com/ufo/cart/module/modules/combat/AimAssist.java
@@ -3,6 +3,7 @@ package com.ufo.cart.module.modules.combat;
 import com.ufo.cart.event.listeners.TickListener;
 import com.ufo.cart.module.Category;
 import com.ufo.cart.module.Module;
+import com.ufo.cart.module.setting.NumberSetting;
 import com.ufo.cart.utils.other.FindClosestPlayer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
@@ -10,9 +11,13 @@ import net.minecraft.util.math.MathHelper;
 public class AimAssist extends Module implements TickListener {
 
     private float yaw, pitch;
+    private final NumberSetting FOV = new NumberSetting("FOV", 0.0, 180.0, 180.0, 1.0);
+    private final NumberSetting Range = new NumberSetting("Range", 0.0, 10.0, 3.0, 1.0);
 
-    public AimAssist() {
+    public AimAssist()
+    {
         super("Aim Assist", "Automatically aims at targets", 0, Category.COMBAT);
+        addSettings(FOV, Range);
     }
 
     @Override
@@ -30,19 +35,36 @@ public class AimAssist extends Module implements TickListener {
     @Override
     public void onTick() {
         if (mc.player != null && mc.world != null) {
-            PlayerEntity closestPlayer = FindClosestPlayer.findClosest(mc.player, 4);
+            PlayerEntity closestPlayer = FindClosestPlayer.findClosest(mc.player, Range.getValue());
 
             if (closestPlayer != null) {
                 double diffX = closestPlayer.getX() - mc.player.getX();
-                double diffY = closestPlayer.getY() + closestPlayer.getEyeHeight(closestPlayer.getPose()) - (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()));
+                double diffY = closestPlayer.getY() + closestPlayer.getEyeHeight(closestPlayer.getPose()) -
+                        (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()));
                 double diffZ = closestPlayer.getZ() - mc.player.getZ();
-                double distance = MathHelper.sqrt((float) (diffX * diffX + diffZ * diffZ));
 
-                yaw = (float) (MathHelper.atan2(diffZ, diffX) * 180.0D / Math.PI) - 90.0F;
-                pitch = (float) -(MathHelper.atan2(diffY, distance) * 180.0D / Math.PI);
+                float playerYaw = mc.player.getYaw();
+                float playerPitch = mc.player.getPitch();
 
-                mc.player.setYaw(yaw);
-                mc.player.setPitch(pitch);
+                double playerDirX = -Math.cos(Math.toRadians(playerPitch)) * Math.sin(Math.toRadians(playerYaw));
+                double playerDirY = -Math.sin(Math.toRadians(playerPitch));
+                double playerDirZ = Math.cos(Math.toRadians(playerPitch)) * Math.cos(Math.toRadians(playerYaw));
+
+                double targetDistance = MathHelper.sqrt((float) (diffX * diffX + diffY * diffY + diffZ * diffZ));
+                double targetDirX = diffX / targetDistance;
+                double targetDirY = diffY / targetDistance;
+                double targetDirZ = diffZ / targetDistance;
+
+                double dotProduct = (playerDirX * targetDirX) + (playerDirY * targetDirY) + (playerDirZ * targetDirZ);
+                double angle = Math.toDegrees(Math.acos(dotProduct));
+
+                float fov = (float) FOV.getValue();
+                if (angle <= fov) {
+                    yaw = (float) (MathHelper.atan2(diffZ, diffX) * 180.0D / Math.PI) - 90.0F;
+                    pitch = (float) -(MathHelper.atan2(diffY, MathHelper.sqrt((float) (diffX * diffX + diffZ * diffZ))) * 180.0D / Math.PI);
+                    mc.player.setYaw(yaw);
+                    mc.player.setPitch(pitch);
+                }
             }
         }
     }
