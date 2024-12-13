11d43b65af71eb8ffc983e637bb2b0af559fe964
ufyu <80692105+ufyo@users.noreply.github.com>
2024-12-12 20:32:14 -0500

    triggerbot update :)
    💩

--- src/main/java/com/ufo/cart/module/modules/combat/TriggerBot.java
+++ src/main/java/com/ufo/cart/module/modules/combat/TriggerBot.java
@@ -22,7 +22,7 @@ public class TriggerBot extends Module implements TickListener {
     private final BooleanSetting swordOnly = new BooleanSetting("Sword Only", false);
     private final RangeSetting attackDelay = new RangeSetting("Attack Delay", 0, 20, 0, 1, 1);
     private int attackCooldown = 0;
-    Random random = new Random();
+    private final Random random = new Random(); // declare random as final so you cant recrate dumbo
 
     public TriggerBot() {
         super("Trigger Bot", "Automatically hits targets", 0, Category.COMBAT);
@@ -55,26 +55,30 @@ public class TriggerBot extends Module implements TickListener {
         if (mc.crosshairTarget instanceof EntityHitResult hit && mc.crosshairTarget.getType() == HitResult.Type.ENTITY) {
             Entity target = hit.getEntity();
 
-            if (attackDelay.getValueMaxInt() != attackDelay.getValueMinInt()) {
-                int attackDelayInterval = random.nextInt((attackDelay.getValueMaxInt() - attackDelay.getValueMinInt()));
-            }
-
             if (target instanceof PlayerEntity && target.isAlive() && target != mc.player) {
                 Item heldItem = mc.player.getMainHandStack().getItem();
+                int delay = 0;
+                int attackDelayInterval = 0;
+
+                if(attackDelay.getValueMaxInt() != attackDelay.getValueMinInt()) {
+                    attackDelayInterval = random.nextInt(attackDelay.getValueMaxInt() - attackDelay.getValueMinInt());
+
+                }
 
                 if (swordOnly.getValue()) {
                     if (!(heldItem instanceof SwordItem)) {
                         return;
                     }
-                    attackCooldown = 16 + attackDelayInterval;
+                    delay = 16 + attackDelayInterval;
                 } else {
                     if (heldItem instanceof SwordItem || heldItem instanceof AxeItem) {
-                        attackCooldown = heldItem instanceof SwordItem ? 16 + attackDelayInterval : 16 + attackDelayInterval; // exmaple dleyas
+                        delay =  16 + attackDelayInterval; // exmaple dleyas
                     } else {
-                        attackCooldown = 4 + attackDelayInterval;
+                        delay = 4 + attackDelayInterval;
                     }
                 }
 
+                attackCooldown = delay;
                 ClientPlayerInteractionManager interactionManager = mc.interactionManager;
                 interactionManager.attackEntity(mc.player, target);
                 mc.player.swingHand(Hand.MAIN_HAND);
