bdd967f908130fe53f7f4f8abb9962e57f2580f9
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-12-16 14:13:39 -0800

    almost

--- src/main/java/com/ufo/cart/module/modules/render/ESP.java
+++ src/main/java/com/ufo/cart/module/modules/render/ESP.java
@@ -4,10 +4,14 @@ import com.ufo.cart.event.events.Render3DEvent;
 import com.ufo.cart.event.listeners.Render3DListener;
 import com.ufo.cart.module.Category;
 import com.ufo.cart.module.Module;
-import com.ufo.cart.utils.render.Render3D;
+import com.ufo.cart.utils.render.RenderUtils;
+import com.ufo.cart.utils.render.ThemeUtils;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
-import java.awt.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 public class ESP extends Module implements Render3DListener {
 
@@ -29,10 +33,33 @@ public class ESP extends Module implements Render3DListener {
 
     @Override
     public void onRender(Render3DEvent event) {
-        assert mc.world != null;
-        for (Entity entity : mc.world.getPlayers()) {
-            Render3D.render3DBox(new MatrixStack(), entity.getBoundingBox(), Color.white, 3);
-            System.out.println("Obamacare");
+        if (mc.world != null && mc.player != null) {
+            for (PlayerEntity player : mc.world.getPlayers()) {
+                if (player != mc.player) {
+                    Camera cam = mc.getBlockEntityRenderDispatcher().camera;
+                    if (cam != null) {
+                        MatrixStack matrices = event.matrices;
+                        matrices.push();
+                        Vec3d vec = cam.getPos();
+                        matrices.translate(-vec.x, -vec.y, -vec.z);
+                    }
+
+                    double xPos = MathHelper.lerp(RenderTickCounter.ONE.getTickDelta(true), player.prevX, player.getX());
+                    double yPos = MathHelper.lerp(RenderTickCounter.ONE.getTickDelta(true), player.prevY, player.getY());
+                    double zPos = MathHelper.lerp(RenderTickCounter.ONE.getTickDelta(true), player.prevZ, player.getZ());
+
+                    RenderUtils.renderFilledBox(
+                            event.matrices,
+                            (float) xPos - player.getWidth() / 2,
+                            (float) yPos,
+                            (float) zPos - player.getWidth() / 2,
+                            (float) xPos + player.getWidth() / 2,
+                            (float) yPos + player.getHeight(),
+                            (float) zPos + player.getWidth() / 2,
+                            ThemeUtils.getMainColor());
+                    event.matrices.pop();
+                }
+            }
         }
     }
 }
