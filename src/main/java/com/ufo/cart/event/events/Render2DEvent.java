f65e73977531cbc40cdfd45827d33c00351a7e4e
ufyu <80692105+ufyo@users.noreply.github.com>
2024-12-16 16:12:24 -0500

    render 2d fix i tink

--- src/main/java/com/ufo/cart/event/events/Render2DEvent.java
+++ src/main/java/com/ufo/cart/event/events/Render2DEvent.java
@@ -4,22 +4,50 @@ import com.ufo.cart.event.Event;
 import com.ufo.cart.event.EventListener;
 import com.ufo.cart.event.listeners.Render2DListener;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
 
 import java.util.ArrayList;
 
 public class Render2DEvent extends Event {
-    public DrawContext ctx;
-    public RenderTickCounter delta;
+    private DrawContext context;
+    private int width;
+    private int height;
 
-    public Render2DEvent(final DrawContext context, final RenderTickCounter delta) {
-        this.ctx = context;
-        this.delta = delta;
+    public Render2DEvent(DrawContext context, int width, int height) {
+        this.context = context;
+        this.width = width;
+        this.height = height;
+    }
+
+    public DrawContext getContext() {
+        return context;
+    }
+
+    public void setContext(DrawContext context) {
+        this.context = context;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     @Override
     public void callListeners(final ArrayList<EventListener> listeners) {
-        listeners.stream().filter(listener -> listener instanceof Render2DListener).map(listener -> (Render2DListener) listener).forEach(listener -> listener.onRender2D(this));
+        listeners.stream()
+            .filter(listener -> listener instanceof Render2DListener)
+            .map(listener -> (Render2DListener) listener)
+            .forEach(listener -> listener.onRender2D(this));
     }
 
     @Override
