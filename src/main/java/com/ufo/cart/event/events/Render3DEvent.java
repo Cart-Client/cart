e654b0473dc2c723c3b804f3f92a830077c83cae
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-12-16 14:22:34 -0800

    SO CLOSEEEEEEE

--- src/main/java/com/ufo/cart/event/events/Render3DEvent.java
+++ src/main/java/com/ufo/cart/event/events/Render3DEvent.java
@@ -4,6 +4,7 @@ import com.ufo.cart.event.Event;
 import com.ufo.cart.event.EventListener;
 import com.ufo.cart.event.listeners.Render3DListener;
 import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
 
 import java.util.ArrayList;
 
@@ -11,7 +12,7 @@ public class Render3DEvent extends Event {
     public MatrixStack matrices;
     public float delta;
 
-    public Render3DEvent(final MatrixStack matrices, final float delta) {
+    public Render3DEvent(final MatrixStack matrices, final float delta, Matrix4f matrix4f) {
         this.matrices = matrices;
         this.delta = delta;
     }
--- src/main/java/com/ufo/cart/mixin/Render3DMixin.java
+++ src/main/java/com/ufo/cart/mixin/Render3DMixin.java
@@ -1,28 +1,38 @@
 package com.ufo.cart.mixin;
 
 import com.ufo.cart.Client;
+import com.ufo.cart.event.Event;
 import com.ufo.cart.event.events.Render3DEvent;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import static com.ufo.cart.Client.mc;
 
 @Mixin(GameRenderer.class)
-public class Render3DMixin {
+public abstract class Render3DMixin {
+    @Shadow public abstract Matrix4f getBasicProjectionMatrix(double fov);
 
-    @Inject(at = @At("HEAD"), method = "render", cancellable = true)
-    private void onRender(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
-        if (mc.world == null || mc.player == null) {
-            return;
-        }
+    @Shadow protected abstract double getFov(Camera camera, float tickDelta, boolean changingFov);
 
+    @Shadow @Final private Camera camera;
+
+    @Shadow @Final private MinecraftClient client;
+
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", ordinal = 1))
+    private void onWorldRender(RenderTickCounter tickCounter, CallbackInfo ci) {
+        double d = getFov(camera, tickCounter.getTickDelta(true), true);
+        Matrix4f matrix4f = getBasicProjectionMatrix(d);
         MatrixStack matrixStack = new MatrixStack();
         float tickDelta = 0;
-        Render3DEvent render3DEvent = new Render3DEvent(matrixStack, tickDelta);
+        Render3DEvent render3DEvent = new Render3DEvent(matrixStack, tickDelta, matrix4f);
         Client.getInstance().getEVENT_BUS().callListeners(render3DEvent);
     }
 }
