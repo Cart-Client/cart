2aff5a2faea503428cd0e563c11b9c7c709844dc
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-12-17 13:47:24 -0800

    added swordonly to aimassist

--- src/main/java/com/ufo/cart/module/modules/combat/AimAssist.java
+++ src/main/java/com/ufo/cart/module/modules/combat/AimAssist.java
@@ -4,8 +4,11 @@ import com.ufo.cart.event.events.Render3DEvent;
 import com.ufo.cart.event.listeners.Render3DListener;
 import com.ufo.cart.module.Category;
 import com.ufo.cart.module.Module;
+import com.ufo.cart.module.setting.BooleanSetting;
 import com.ufo.cart.module.setting.NumberSetting;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.math.MathHelper;
 import com.ufo.cart.utils.other.PlayerUtil;
 
@@ -15,10 +18,11 @@ public class AimAssist extends Module implements Render3DListener {
     private final NumberSetting FOV = new NumberSetting("FOV", 0.0, 180.0, 180.0, 1.0);
     private final NumberSetting Range = new NumberSetting("Range", 0.0, 10.0, 3.0, 1.0);
     private final NumberSetting Smoothness = new NumberSetting("Smoothness", 0.0, 1.0, 1.0, 0.05);
+    private final BooleanSetting swordOnly = new BooleanSetting("Sword Only", false);
 
     public AimAssist() {
         super("Aim Assist", "Automatically aims at targets", 0, Category.COMBAT);
-        addSettings(FOV, Range, Smoothness);
+        addSettings(FOV, Range, Smoothness, swordOnly);
     }
 
     @Override
@@ -36,41 +40,50 @@ public class AimAssist extends Module implements Render3DListener {
     @Override
     public void onRender(Render3DEvent event) {
         if (mc.player != null && mc.world != null) {
-            PlayerEntity closestPlayer = PlayerUtil.findClosest(mc.player, Range.getValue());
 
-            if (closestPlayer != null && mc.player.distanceTo(closestPlayer) <= Range.getValue()) {
-                double diffX = closestPlayer.getX() - mc.player.getX();
-                double diffY = (closestPlayer.getY() + closestPlayer.getEyeHeight(mc.player.getPose()) - (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose())));
-                double diffZ = closestPlayer.getZ() - mc.player.getZ();
+            Item heldItem = mc.player.getMainHandStack().getItem();
 
-                float playerYaw = mc.player.getYaw();
-                float playerPitch = mc.player.getPitch();
+            if (swordOnly.getValue()) {
+                if (!(heldItem instanceof SwordItem)) {
+                    return;
+                }
+            }
 
-                double distance = MathHelper.sqrt((float) (diffX * diffX + diffZ * diffZ));
+                PlayerEntity closestPlayer = PlayerUtil.findClosest(mc.player, Range.getValue());
 
-                float targetYaw = (float) (MathHelper.atan2(diffZ, diffX) * 180.0D / Math.PI) - 90.0F;
-                float targetPitch = (float) -(MathHelper.atan2(diffY, distance) * 180.0D / Math.PI);
+                if (closestPlayer != null && mc.player.distanceTo(closestPlayer) <= Range.getValue()) {
+                    double diffX = closestPlayer.getX() - mc.player.getX();
+                    double diffY = (closestPlayer.getY() + closestPlayer.getEyeHeight(mc.player.getPose()) - (mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose())));
+                    double diffZ = closestPlayer.getZ() - mc.player.getZ();
 
-                float yawDiff = MathHelper.wrapDegrees(targetYaw - playerYaw);
-                float pitchDiff = MathHelper.wrapDegrees(targetPitch - playerPitch);
+                    float playerYaw = mc.player.getYaw();
+                    float playerPitch = mc.player.getPitch();
 
-                double angle = Math.toDegrees(Math.acos(MathHelper.clamp(Math.cos(Math.toRadians(yawDiff)) * Math.cos(Math.toRadians(pitchDiff)), -1.0, 1.0)));
+                    double distance = MathHelper.sqrt((float) (diffX * diffX + diffZ * diffZ));
 
-                if (angle <= FOV.getValue()) {
-                    float smoothingFactor = 1.0f - Smoothness.getValueFloat();
+                    float targetYaw = (float) (MathHelper.atan2(diffZ, diffX) * 180.0D / Math.PI) - 90.0F;
+                    float targetPitch = (float) -(MathHelper.atan2(diffY, distance) * 180.0D / Math.PI);
 
-                    smoothingFactor = MathHelper.clamp(smoothingFactor, 0.1f, 1.0f);
+                    float yawDiff = MathHelper.wrapDegrees(targetYaw - playerYaw);
+                    float pitchDiff = MathHelper.wrapDegrees(targetPitch - playerPitch);
 
-                    float aimAssistYawAdjustment = yawDiff * smoothingFactor;
-                    float aimAssistPitchAdjustment = pitchDiff * smoothingFactor;
+                    double angle = Math.toDegrees(Math.acos(MathHelper.clamp(Math.cos(Math.toRadians(yawDiff)) * Math.cos(Math.toRadians(pitchDiff)), -1.0, 1.0)));
 
-                    yaw = mc.player.getYaw() + aimAssistYawAdjustment;
-                    pitch = MathHelper.clamp(mc.player.getPitch() + aimAssistPitchAdjustment, -90.0F, 90.0F);
+                    if (angle <= FOV.getValue()) {
+                        float smoothingFactor = 1.0f - Smoothness.getValueFloat();
 
-                    mc.player.setYaw(yaw);
-                    mc.player.setPitch(pitch);
+                        smoothingFactor = MathHelper.clamp(smoothingFactor, 0.1f, 1.0f);
+
+                        float aimAssistYawAdjustment = yawDiff * smoothingFactor;
+                        float aimAssistPitchAdjustment = pitchDiff * smoothingFactor;
+
+                        yaw = mc.player.getYaw() + aimAssistYawAdjustment;
+                        pitch = MathHelper.clamp(mc.player.getPitch() + aimAssistPitchAdjustment, -90.0F, 90.0F);
+
+                        mc.player.setYaw(yaw);
+                        mc.player.setPitch(pitch);
+                    }
                 }
             }
         }
     }
-}
