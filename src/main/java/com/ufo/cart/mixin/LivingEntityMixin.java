7b11f92315d310e365509cb28ad3edda3df46fb6
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-12-16 21:26:04 -0800

    glowesp

---
+++ src/main/java/com/ufo/cart/mixin/LivingEntityMixin.java
@@ -0,0 +1,18 @@
+package com.ufo.cart.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+    @Inject(method = "tick", at = @At("TAIL"))
+    void onTick(CallbackInfo callbackInfo) {
+        if ((Object) this instanceof PlayerEntity) {
+            ((LivingEntity) (Object) this).setGlowing(true);
+        }
+    }
+}
