493776b2ef7b953fa58790809470db00bf38076b
ufyu <80692105+ufyo@users.noreply.github.com>
2024-12-17 00:28:15 -0500

    Update LivingEntityMixin.java

--- src/main/java/com/ufo/cart/mixin/LivingEntityMixin.java
+++ src/main/java/com/ufo/cart/mixin/LivingEntityMixin.java
@@ -1,18 +1,44 @@
 package com.ufo.cart.mixin;
 
+import com.ufo.cart.Client;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Pair;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
-    @Inject(method = "tick", at = @At("TAIL"))
-    void onTick(CallbackInfo callbackInfo) {
-        if ((Object) this instanceof PlayerEntity) {
-            ((LivingEntity) (Object) this).setGlowing(true);
+@Mixin(LivingEntityMixin.class)
+public class LivingEntityMixin<T extends LivingEntity> {
+
+    private final ThreadLocal<Pair<Float, Float>> rotationPitch = ThreadLocal.withInitial(() -> null);
+
+    @Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"))
+    private void renderInject(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        Pair<Float, Float> rotationPitch = Client.INSTANCE.getRotationManager().rotationPitch;
+
+        this.rotationPitch.set(null);
+
+        if (livingEntity != MinecraftClient.getInstance().player) {
+            return;
+        }
+
+        this.rotationPitch.set(new Pair<>(rotationPitch.getLeft(), rotationPitch.getRight()));
+    }
+
+    @Redirect(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", ordinal = 0))
+    private float renderInject(float g, float f, float s) {
+        Pair<Float, Float> rot = this.rotationPitch.get();
+        if (rot != null) {
+            return MathHelper.lerp(g, rot.getLeft(), rot.getRight());
+        } else {
+            return MathHelper.lerp(g, f, s);
         }
     }
 }
