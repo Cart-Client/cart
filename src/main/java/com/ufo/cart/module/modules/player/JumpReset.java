1d42529b2370db5bc637bca18d1672eb5364d28a
DellOptiplexFan <Michael.D.Schneider@proton.me>
2024-12-23 08:47:43 -0800

    Update JumpReset.java

--- src/main/java/com/ufo/cart/module/modules/player/JumpReset.java
+++ src/main/java/com/ufo/cart/module/modules/player/JumpReset.java
@@ -3,10 +3,17 @@ package com.ufo.cart.module.modules.player;
 import com.ufo.cart.event.listeners.TickListener;
 import com.ufo.cart.module.Category;
 import com.ufo.cart.module.Module;
+import com.ufo.cart.module.setting.NumberSetting;
+
+import static com.ufo.cart.utils.math.MathUtils.random;
 
 public final class JumpReset extends Module implements TickListener {
+
+    public final NumberSetting chance = new NumberSetting("Chance", 0, 100, 50, 1);
+
     public JumpReset() {
-        super("Jump Reset", "Jumps when hit to reduce incoming knockback.", 0, Category.PLAYER);
+        super("Jump Reset", "Jumps when hit to reduce incoming knock back.", 0, Category.PLAYER);
+
     }
 
     @Override
@@ -23,8 +30,24 @@ public final class JumpReset extends Module implements TickListener {
 
     @Override
     public void onTick() {
-        if (mc.player != null) {
-            if (mc.player.isOnGround() && mc.player.hurtTime > 0) {
+        assert mc.player != null;
+        if (random.nextInt(1, 100) <= chance.getValueInt()) {
+            if (mc.currentScreen != null)
+                return;
+
+            if (mc.player.isUsingItem())
+                return;
+
+            if (mc.player.hurtTime == 0)
+                return;
+
+            if (mc.player.hurtTime == mc.player.maxHurtTime)
+                return;
+
+            if (!mc.player.isOnGround())
+                return;
+
+            if (mc.player.hurtTime == 9 && random.nextInt(1, 100) <= chance.getValueInt()) {
                 mc.player.jump();
             }
         }
